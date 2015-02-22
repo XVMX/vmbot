6fa2666c6d8765d25079d00fe8eaea8bde6662db
Sascha J <sjuengling@gmail.com>
2015-02-22 03:41:45 +0100

    Added arelesay

--- vmbot.py
+++ vmbot.py
@@ -135,6 +135,7 @@ class VMBot(MUCJabberBot):
     pimpisms = ["eabod","why do you hate black people?", "i want a bucket full of money covered rainbows","bundle of sticks"]
     chaseisms = ["would you PLEASE"]
     nickisms = ["D00d!", "But d00d!", "Come on d00d...", "Oh d00d", "D0000000000000000000000000000000d!", "D00d, never go full retart!"]
+    areleisms = ["5R8At-Qno_o", "MZwXoDyj9Vc", "So9LshyaHd0", "sW4JRSzPJQo", "evCj25UtfLM", "aneUrHBRyhg", "9XHqg7mTizE", "gtM9xD-Ky7E"]
     directors = ["jack_haydn", "thirteen_fish", "pimpin_yourhos", "johann_tollefson", "petyr_baelich", "ektony", "kairk_efraim", "lofac", "jons_squire"]
     admins = ["jack_haydn", "thirteen_fish"]
     access_token = ''
@@ -591,6 +592,11 @@ class VMBot(MUCJabberBot):
             return " " + args + " " + random.choice(self.pimpisms)
         else:
             return random.choice(self.pimpisms)
+    
+    @botcmd
+    def arelesay(self, mess, args):
+        '''Like fishsay but more fucked up'''
+        return "https://www.youtube.com/watch?v=" + random.choice(self.pimpisms)
 
     @botcmd
     def nicksay(self, mess, args):
