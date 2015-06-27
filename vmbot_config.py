c08d66a4605bc0ce4de68e6b4d61dc8c5af49353
ThirteenFish <ThirteenFish@server.fake>
2015-06-26 18:16:43 -0700

    Added a non-executable config file
    Since we're modifying the config anyway we might as well do it
    right(ish). This way everyone can have their own vmbot.cfg and wont run
    the risk of merge conflicts or accidentally pushing sensitive data.

--- vmbot_config.py
+++ vmbot_config.py
@@ -1,23 +1,29 @@
+import ConfigParser
+
+
+parser = ConfigParser.SafeConfigParser()
+parser.read('vmbot.cfg')
+
 # Jabber configuration
-username  = ''
-password  = ''
-res       = ''
-nickname  = ''
-chatroom1 = ''
-chatroom2 = ''
-chatroom3 = ''
+username  = parser.get('Jabber', 'username')
+password  = parser.get('Jabber', 'password')
+res       = parser.get('Jabber', 'resource')
+nickname  = parser.get('Jabber', 'nickname')
+chatroom1 = parser.get('Jabber', 'chatroom1')
+chatroom2 = parser.get('Jabber', 'chatroom2')
+chatroom3 = parser.get('Jabber', 'chatroom3')
 
 # GSF broadcast API
-url    = ''
-id     = ''
-key    = ''
-target = ''
+url    = parser.get('GSF Broadcast', 'url')
+id     = parser.get('GSF Broadcast', 'id')
+key    = parser.get('GSF Broadcast', 'key')
+target = parser.get('GSF Broadcast', 'target')
 
 # RC blacklist API
-blurl = ''
-blkey = ''
+blurl = parser.get('RC Blacklist', 'url')
+blkey = parser.get('RC Blacklist', 'key')
 
 # SSO-CREST
-client_id     = ''
-client_secret = ''
-refresh_token = ''
+client_id     = parser.get('SSO-CREST', 'client_id')
+client_secret = parser.get('SSO-CREST', 'client_secret')
+refresh_token = parser.get('SSO-CREST', 'refresh_token')
