dab7f968c8378cceacfd9af4477b2d06dcee2fcf
TheJokr <leo@dashsec.com>
2016-01-24 22:18:18 +0100

    Refactored fun.py

--- vmbot/fun.py
+++ vmbot/fun.py
@@ -9,26 +9,26 @@ from bs4 import BeautifulSoup
 class Say(object):
     # 8ball answers like the original, as per http://en.wikipedia.org/wiki/Magic_8-Ball
     eball_answers = [
-        'It is certain',
-        'It is decidedly so',
-        'Without a doubt',
-        'Yes definitely',
-        'You may rely on it',
-        'As I see it, yes',
-        'Most likely',
-        'Outlook good',
-        'Yes',
-        'Signs point to yes',
-        'Reply hazy try again',
-        'Ask again later',
-        'Better not tell you now',
-        'Cannot predict now',
-        'Concentrate and ask again',
-        'Don\'t count on it',
-        'My reply is no',
-        'My sources say no',
-        'Outlook not so good',
-        'Very doubtful'
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
     ]
     fishisms = [
         "~The Python Way!~",
@@ -43,16 +43,6 @@ class Say(object):
         "bundle of sticks",
         "that went over like a jerrys kids rodeo with live bulls"
     ]
-    chaseisms = [
-        "would you PLEASE"
-    ]
-    nickisms = [
-        "D00d!",
-        "But d00d!",
-        "Come on d00d...",
-        "Oh d00d",
-        "D00d, never go full retart!"
-    ]
     areleisms = [
         "PzbjWI3EhyI",
         "5R8At-Qno_o",
@@ -64,6 +54,16 @@ class Say(object):
         "9XHqg7mTizE",
         "gtM9xD-Ky7E",
         "ZTidn2dBYbY"
+    ]
+    nickisms = [
+        "D{}d!",
+        "But d{}d!",
+        "Come on d{}d...",
+        "Oh d{}d",
+        "D{}d, never go full retart!"
+    ]
+    chaseisms = [
+        "would you PLEASE"
     ]
     kairkisms = [
         "thanks for filling this out.",
@@ -85,12 +85,12 @@ class Say(object):
 
     @botcmd
     def fishsay(self, mess, args):
-        'Fishy wisdom.'
+        """Fishy wisdom"""
         return random.choice(self.fishisms)
 
     @botcmd
     def pimpsay(self, mess, args):
-        'Like fishsay but blacker'
+        """Like fishsay but blacker"""
         if args:
             return "{} {}".format(args, random.choice(self.pimpisms))
         else:
@@ -98,74 +98,74 @@ class Say(object):
 
     @botcmd
     def arelesay(self, mess, args):
-        'Like fishsay but more fuckey'
-        return "https://youtu.be/" + random.choice(self.areleisms)
+        """Like fishsay but more fuckey"""
+        return "https://youtu.be/{}".format(random.choice(self.areleisms))
 
     @botcmd
     def nicksay(self, mess, args):
-        'Like fishsay but pubbietasticer'
-        nickrandom = random.choice(self.nickisms)
-        return nickrandom.replace('00', '0' * int(2 + random.expovariate(.25)))
+        """Like fishsay but pubbietasticer"""
+        return random.choice(self.nickisms).format('0' * int(2 + random.expovariate(.25)))
 
     @botcmd
     def chasesay(self, mess, args):
-        'Please'
-        cmdname = self.chasesay._jabberbot_command_name
-        if args.startswith(cmdname):
+        """Please"""
+        if args.startswith(self.chasesay._jabberbot_command_name):
             return "nope"
-        sender = (args.strip() if args else self.get_sender_username(mess))
+        sender = args.strip() if args else self.get_sender_username(mess)
         return "{}, {}".format(sender, self.chaseisms[0])
 
     @botcmd
     def kairksay(self, mess, args):
-        'Like fishsay but more Kafkaesque'
-        sender = (args.strip() if args else self.get_sender_username(mess))
+        """Like fishsay but more Kafkaesque"""
+        sender = args.strip() if args else self.get_sender_username(mess)
         return "{}, {} -Kairk".format(sender, random.choice(self.kairkisms))
 
     @botcmd
     def dariussay(self, mess, args):
-        'Like fishsay but bordering on weird'
-        sender = (args.strip() if args else self.get_sender_username(mess))
+        """Like fishsay but bordering on weird"""
+        sender = args.strip() if args else self.get_sender_username(mess)
         return "{}, {}".format(sender, random.choice(self.dariusisms))
 
     @botcmd
-    def eksay(self, mess, args):
-        'Like fishsay but more dead'
-        sender = (args.strip() if args else self.get_sender_username(mess))
-        return ":rip: {}".format(sender)
+    def scottsay(self, mess, args):
+        """Like fishsay but coming from Israel"""
+        if args:
+            return "{}, {}".format(args, random.choice(self.scottisms))
+        else:
+            return random.choice(self.scottisms)
 
     @botcmd
-    def scottsay(self, mess, args):
-        'Like fishsay but more profitable'
-        sender = (args.strip() if args else self.get_sender_username(mess))
-        return "{}, {}".format(sender, random.choice(self.scottisms))
+    def eksay(self, mess, args):
+        """Like fishsay but more dead"""
+        sender = args.strip() if args else self.get_sender_username(mess)
+        return ":rip: {}".format(sender)
 
     @botcmd(name="8ball")
     def bot_8ball(self, mess, args):
-        '<question> - Provides insight into the future'
+        """<question> - Provides insight into the future"""
         if not args:
-            return 'You will need to provide a question for me to answer.'
+            return "You will need to provide a question for me to answer"
         else:
             return random.choice(self.eball_answers)
 
     @botcmd
     def sayhi(self, mess, args):
-        '[name] - Says hi to you or name if provided!'
-        sender = (args.strip() if args else self.get_sender_username(mess))
+        """[name] - Says hi to you or name if provided"""
+        sender = args.strip() if args else self.get_sender_username(mess)
         return "Hi {}!".format(sender)
 
 
 class Fun(object):
     @botcmd
     def rtd(self, mess, args):
-        '''Like a box of chocolates, you never know what you're gonna get'''
-        with open("data/emotes.txt", 'r') as emotes:
-            remotes = emotes.read().split('\n')
+        """Like a box of chocolates, you never know what you're gonna get"""
+        with open("data/emotes.txt", 'r') as emotesFile:
+            emotes = emotesFile.read().split('\n')
 
-        while not remotes.pop(0).startswith('[default]'):
+        while not emotes.pop(0).startswith("[default]"):
             pass
 
-        return random.choice(remotes).split()[-1]
+        return random.choice(emotes).split()[-1]
 
     @botcmd
     def rtq(self, mess, args):
@@ -188,7 +188,7 @@ class Fun(object):
 
     @botcmd
     def rtxkcd(self, mess, args):
-        '''Like a box of chocolates, but with xkcd'''
+        """Like a box of chocolates, but with xkcds"""
         res = requests.get("https://xkcd.com/info.0.json").json()
         comicID = random.randint(1, res['num'])
 
@@ -198,62 +198,62 @@ class Fun(object):
 
 
 class Chains(object):
-    @botcmd(hidden=True)
-    def every(self, mess, args):
-        '''Every lion except for at most one'''
+    @botcmd(hidden=True, name="every")
+    def bot_every(self, mess, args):
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return "lion"
 
-    @botcmd(hidden=True)
-    def lion(self, mess, args):
-        '''Every lion except for at most one'''
+    @botcmd(hidden=True, name="lion")
+    def bot_lion(self, mess, args):
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return "except"
 
     @botcmd(hidden=True, name="except")
     def bot_except(self, mess, args):
-        '''Every lion except for at most one'''
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return "for"
 
     @botcmd(hidden=True, name="for")
     def bot_for(self, mess, args):
-        '''Every lion except for at most one'''
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return "at"
 
-    @botcmd(hidden=True)
-    def at(self, mess, args):
-        '''Every lion except for at most one'''
+    @botcmd(hidden=True, name="at")
+    def bot_at(self, mess, args):
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return "most"
 
     @botcmd(hidden=True, name="most")
     def bot_most(self, mess, args):
-        '''Every lion except for at most one'''
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return "one"
 
     @botcmd(hidden=True, name="one")
     def bot_one(self, mess, args):
-        '''Every lion except for at most one'''
+        """Every lion except for at most one"""
         if not args and random.randint(1, 5) == 1:
             return ":bravo:"
 
-    @botcmd(hidden=True, name="z")
+    @botcmd(hidden=True, name='z')
     def bot_z(self, mess, args):
-        '''z0r'''
+        """z0r"""
         if not args and random.randint(1, 3) == 1:
-            return "0"
+            return '0'
 
-    @botcmd(hidden=True, name="0")
+    @botcmd(hidden=True, name='0')
     def bot_0(self, mess, args):
-        '''z0r'''
+        """z0r"""
         if not args and random.randint(1, 3) == 1:
-            return "r"
+            return 'r'
 
-    @botcmd(hidden=True, name="r")
+    @botcmd(hidden=True, name='r')
     def bot_r(self, mess, args):
-        '''z0r'''
+        """z0r"""
         if not args and random.randint(1, 3) == 1:
-            return "z"
+            return 'z'
