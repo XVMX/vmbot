ccc60cacf2757ed29f1c576c626e99cd18152f2f
TheJokr <leo@dashsec.com>
2016-02-18 19:46:28 +0100

    Added date to runBotForever.sh log

--- tools/runBotForever.sh
+++ tools/runBotForever.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-    python ./vmbot/vmbot.py > ./info.vmbot.log 2> ./err.vmbot.log
-    echo "\"vmbot.py\" exited with code $?. Restarting in 5 seconds..." >&2
+    python ./vmbot/vmbot.py > ./out.vmbot.log 2> ./err.vmbot.log
+    echo "$(date "+%b %d %H:%M:%S"): \"vmbot.py\" exited with code $?. Restarting in 5 seconds..." >&2
     sleep 5
 done
