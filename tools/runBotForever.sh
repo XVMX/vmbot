92686ba14eca861713dcef18fbc549ca07d7e652
TheJokr <leo@dashsec.com>
2016-04-16 21:07:26 +0200

    Added individual logfiles for each startup
    The current logfile won't be overridden after a bot restart anymore

--- tools/runBotForever.sh
+++ tools/runBotForever.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
-    python ./vmbot/vmbot.py > ./out.vmbot.log 2> ./err.vmbot.log
+    python ./vmbot/vmbot.py 2> ./`date "+%Y-%m-%d-%H%M"`.vmbot.log
     echo "$(date "+%b %d %H:%M:%S"): \"vmbot.py\" exited with code $?. Restarting in 5 seconds..." >&2
     sleep 5
 done
