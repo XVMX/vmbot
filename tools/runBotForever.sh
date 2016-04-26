e7cd0ee670dbad006d7c7a430ed28011cbf8252b
TheJokr <leo@dashsec.com>
2016-04-26 15:32:15 +0200

    Reduced restart timeout to 3 seconds

--- tools/runBotForever.sh
+++ tools/runBotForever.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
     python -m vmbot
-    echo "$(date "+%b %d %H:%M:%S"): VMBot exited with code $?. Restarting in 5 seconds..." >&2
-    sleep 5
+    echo "$(date "+%b %d %H:%M:%S"): VMBot exited with code $?. Restarting in 3 seconds..." >&2
+    sleep 3
 done
