33e1472a5fa058a1757ef99ba66248838c3cbe1a
TheJokr <leo@dashsec.com>
2016-04-26 19:34:10 +0200

    Refactored config tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,52 +1,51 @@
 import unittest
 
+import os
+
+from vmbot.helpers.files import CONFIG
+
 from vmbot.config import config
 
 
+@unittest.skipUnless(os.path.isfile(CONFIG), "Config file not found")
 class TestConfig(unittest.TestCase):
-    def setUp(self):
-        self.config = config
-
-    def tearDown(self):
-        del self.config
-
     def test_loglevel(self):
-        self.assertEqual(self.config['loglevel'], "INFO")
+        self.assertEqual(config['loglevel'], "INFO")
 
     def test_username(self):
-        self.assertEqual(self.config['jabber']['username'], "username@domain.tld")
+        self.assertEqual(config['jabber']['username'], "username@domain.tld")
 
     def test_password(self):
-        self.assertEqual(self.config['jabber']['password'], "yourpassword")
+        self.assertEqual(config['jabber']['password'], "yourpassword")
 
     def test_resource(self):
-        self.assertEqual(self.config['jabber']['res'], "VMBot")
+        self.assertEqual(config['jabber']['res'], "VMBot")
 
     def test_nickname(self):
-        self.assertEqual(self.config['jabber']['nickname'], "BotNickname")
+        self.assertEqual(config['jabber']['nickname'], "BotNickname")
 
     def test_chatrooms(self):
-        self.assertEqual(self.config['jabber']['chatrooms'][0], "room1@conference.domain.tld")
-        self.assertEqual(self.config['jabber']['chatrooms'][1], "room2@conference.domain.tld")
-        self.assertEqual(self.config['jabber']['chatrooms'][2], "room3@conference.domain.tld")
+        self.assertEqual(config['jabber']['chatrooms'][0], "room1@conference.domain.tld")
+        self.assertEqual(config['jabber']['chatrooms'][1], "room2@conference.domain.tld")
+        self.assertEqual(config['jabber']['chatrooms'][2], "room3@conference.domain.tld")
 
     def test_bcast_url(self):
-        self.assertEqual(self.config['bcast']['url'], "")
+        self.assertEqual(config['bcast']['url'], "")
 
     def test_bcast_id(self):
-        self.assertEqual(self.config['bcast']['id'], "")
+        self.assertEqual(config['bcast']['id'], "")
 
     def test_bcast_key(self):
-        self.assertEqual(self.config['bcast']['key'], "")
+        self.assertEqual(config['bcast']['key'], "")
 
     def test_bcast_target(self):
-        self.assertEqual(self.config['bcast']['target'], "")
+        self.assertEqual(config['bcast']['target'], "")
 
     def test_blacklist_url(self):
-        self.assertEqual(self.config['blacklist']['url'], "")
+        self.assertEqual(config['blacklist']['url'], "")
 
     def test_blacklist_key(self):
-        self.assertEqual(self.config['blacklist']['key'], "")
+        self.assertEqual(config['blacklist']['key'], "")
 
 
 if __name__ == "__main__":
