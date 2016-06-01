ecb70a6e621f28409186a786bf9a3ae9da9489a5
TheJokr <leo@dashsec.com>
2016-06-01 15:21:55 +0200

    Added curl as primary download tool
    Wget is used as fallback if curl is not found

---
+++ tools/create_staticdata.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+if command -v curl > /dev/null 2>&1; then
+    get="curl -LO"
+elif command -v wget > /dev/null 2>&1; then
+    get="wget"
+else
+    echo "Failed to locate network downloader" >&2
+    exit 1
+fi
+
+$get https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2
+$get https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2.md5
+
+echo "***"
+echo "MD5 check"
+echo "***"
+if ! md5sum -c sqlite-latest.sqlite.bz2.md5; then
+    rm sqlite-latest.sqlite.bz2 sqlite-latest.sqlite.bz2.md5
+    exit 1
+fi
+
+echo "***"
+echo "Extracting archive"
+echo "***"
+bunzip2 -v sqlite-latest.sqlite.bz2
+
+echo "***"
+echo "Creating new database"
+echo "***"
+sqlite3 sqlite-latest.sqlite ".dump mapRegions" > dump.sql
+sqlite3 sqlite-latest.sqlite ".dump mapConstellations" >> dump.sql
+sqlite3 sqlite-latest.sqlite ".dump mapSolarSystems" >> dump.sql
+sqlite3 sqlite-latest.sqlite ".dump invTypes" >> dump.sql
+sqlite3 staticdata.sqlite < dump.sql
+
+echo "***"
+echo "Deleting temporary/downloaded files"
+echo "***"
+rm dump.sql sqlite-latest.sqlite sqlite-latest.sqlite.bz2.md5
+
+echo "***"
+echo "Finished"
+echo "***"
