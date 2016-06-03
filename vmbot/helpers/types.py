e772fb9e9f104ffb91c126f1db4460dab92cce03
TheJokr <leo@dashsec.com>
2016-06-03 21:37:19 +0200

    Refactored custom types

--- vmbot/helpers/types.py
+++ vmbot/helpers/types.py
@@ -1,9 +1,10 @@
 class ISK(float):
     """Represent ISK values."""
+
     def __format__(self, format_spec):
         """Format the ISK value with commonly used prefixes."""
         for unit in ['', 'k', 'm', 'b']:
             if self < 1000:
-                return "{}{}".format(format(self, format_spec), unit)
+                return format(self, format_spec) + unit
             self /= 1000
-        return "{}t".format(format(self, format_spec))
+        return format(self, format_spec) + 't'
