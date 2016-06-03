c3bba0deabb8b220f28efabb93744adf8fd61a1b
TheJokr <leo@dashsec.com>
2016-06-03 21:36:56 +0200

    Refactored file path helper

--- vmbot/helpers/files.py
+++ vmbot/helpers/files.py
@@ -1,10 +1,9 @@
 from os import path, pardir
 
-
 _DATADIR = path.abspath(path.join(path.dirname(__file__), pardir, "data"))
 
 CONFIG = path.join(_DATADIR, "vmbot.cfg")
-STATICDATA_DB = path.join(_DATADIR, "staticdata.sqlite")
 EMOTES = path.join(_DATADIR, "emotes.txt")
-CACHE_DB = path.join(_DATADIR, "cache.db")
+STATICDATA_DB = path.join(_DATADIR, "staticdata.sqlite")
 WH_DB = path.join(_DATADIR, "wh.db")
+CACHE_DB = path.join(_DATADIR, "cache.db")
