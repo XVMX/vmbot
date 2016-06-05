793627a3f7bfbf29b28eac64c0f9b05e5d5ce3eb
TheJokr <leo@dashsec.com>
2016-06-05 23:40:32 +0200

    Made botcmd PEP 8 compliant
    Function arguments should be all lowercase

--- vmbot/botcmd.py
+++ vmbot/botcmd.py
@@ -2,8 +2,8 @@ from . import jabberbot
 
 
 def botcmd(*args, **kwargs):
-    def decorate(func, forcePM=False, **kwargs):
-        setattr(func, "_vmbot_forcepm", forcePM)
+    def decorate(func, force_pm=False, **kwargs):
+        setattr(func, "_vmbot_forcepm", force_pm)
         return jabberbot.botcmd(func, **kwargs)
 
     if args:
