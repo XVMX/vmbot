82794fe620d179a2411f09c408483fcc4dad9d29
TheJokr <leo@dashsec.com>
2016-06-08 17:04:48 +0200

    Refactored wh

--- vmbot/wh.py
+++ vmbot/wh.py
@@ -1,11 +1,9 @@
-from os import path
 import shlex
 import sqlite3
 
 from .botcmd import botcmd
 from .helpers.files import WH_DB, STATICDATA_DB
 from .helpers.exceptions import DBError
-
 from .helpers import api
 
 
@@ -66,25 +64,25 @@ class Wormhole(object):
         +-> TTL: Minimum number of hours left before the WH closes
         stats - Shows a list of scanners and how many WHs they have scanned during the last 30 days
         """
-        argsList = shlex.split(args)
-        if argsList:
-            cmd = argsList.pop(0).upper()
+        args_list = shlex.split(args)
+        if args_list:
+            cmd = args_list.pop(0).upper()
         else:
             return "Requires one of list, filter, add or stats as an argument"
-        argc = len(argsList)
+        argc = len(args_list)
 
         if cmd == "LIST" and argc == 0:
             return self.wh_list(mess)
         elif cmd == "FILTER" and argc == 2:
-            return self.wh_filter(mess, *argsList)
+            return self.wh_filter(mess, *args_list)
         elif cmd == "ADD" and argc == 5:
-            return self.wh_add(mess, *argsList)
+            return self.wh_add(mess, *args_list)
         elif cmd == "STATS" and argc == 0:
             return self.wh_stats(mess)
         else:
             return "wh {} is not an accepted command".format(args)
 
-    def _getActiveConnections(self):
+    def _get_active_connections(self):
         return self.__db_connection().execute(
             """SELECT SRC, `SRC-SIG`, DEST, `DEST-SIG`, author,
                       (JULIANDAY(expiry) - JULIANDAY("now")) * 24 AS TTL
@@ -95,16 +93,17 @@ class Wormhole(object):
     def wh_list(self, mess, data=None):
         if data is None:
             try:
-                data = self._getActiveConnections()
+                data = self._get_active_connections()
             except sqlite3.OperationalError:
                 return "Error: Data is missing"
 
         connections = []
-        connectionDetails = "{} ({} | {}) <-> {} ({} | {}) | About {:.0f}h left | Scanned by {}"
+        desc = "{} ({} | {}) <-> {} ({} | {}) | About {:.0f}h left | Scanned by {}"
         for connection in data:
             src = api.get_solarSystemData(connection['SRC'])
             dest = api.get_solarSystemData(connection['DEST'])
-            connections.append(connectionDetails.format(
+
+            connections.append(desc.format(
                 src['solarSystemName'], connection['SRC-SIG'], src['regionName'],
                 dest['solarSystemName'], connection['DEST-SIG'], dest['regionName'],
                 float(connection['TTL']), connection['author']
@@ -115,24 +114,24 @@ class Wormhole(object):
 
         return "No connections found"
 
-    def wh_filter(self, mess, filterType, filterVal):
-        filterType, filterVal = filterType.upper(), filterVal.upper()
-        if filterType == "TTL":
+    def wh_filter(self, mess, filter_type, filter_val):
+        filter_type, filter_val = filter_type.upper(), filter_val.upper()
+        if filter_type == "TTL":
             try:
-                filterVal = float(filterVal)
-            except:
+                filter_val = float(filter_val)
+            except ValueError:
                 return "value must be a floating point number when used with TTL"
 
         try:
-            data = self._getActiveConnections()
+            data = self._get_active_connections()
         except sqlite3.OperationalError:
             return "Error: Data is missing"
 
-        allConnections = []
+        connections = []
         for connection in data:
             src = api.get_solarSystemData(connection['SRC'])
             dest = api.get_solarSystemData(connection['DEST'])
-            allConnections.append({
+            connections.append({
                 'SRC': connection['SRC'],
                 'SRC-System': src['solarSystemName'],
                 'SRC-Region': src['regionName'],
@@ -145,22 +144,20 @@ class Wormhole(object):
                 'author': connection['author']
             })
 
-        if filterType == "TTL":
-            filteredConnections = [wh for wh in allConnections if wh['TTL'] >= filterVal]
-        elif filterType == "SYSTEM":
-            filteredConnections = [wh for wh in allConnections
-                                   if filterVal in (wh['SRC-System'].upper(),
-                                                    wh['SRC-Region'].upper(),
-                                                    wh['DEST-System'].upper(),
-                                                    wh['DEST-Region'].upper())]
-        else:
-            filteredConnections = allConnections
+        if filter_type == "TTL":
+            connections = [wh for wh in connections if wh['TTL'] >= filter_val]
+        elif filter_type == "SYSTEM":
+            connections = [wh for wh in connections
+                           if filter_val in (wh['SRC-System'].upper(),
+                                             wh['SRC-Region'].upper(),
+                                             wh['DEST-System'].upper(),
+                                             wh['DEST-Region'].upper())]
 
-        return self.wh_list(mess, filteredConnections)
+        return self.wh_list(mess, connections)
 
-    def wh_add(self, mess, src, srcSIG, dest, destSIG, TTL):
+    def wh_add(self, mess, src, src_sig, dest, dest_sig, ttl):
         try:
-            TTL = float(TTL)
+            ttl = float(ttl)
         except ValueError:
             return "TTL must be a floating point number"
 
@@ -171,13 +168,13 @@ class Wormhole(object):
             return str(e)
 
         staticdata = sqlite3.connect(STATICDATA_DB)
-        srcSystems = staticdata.execute(
+        src_systems = staticdata.execute(
             """SELECT solarSystemID, solarSystemName
                FROM mapSolarSystems
                WHERE solarSystemName LIKE :name;""",
             {'name': "%{}%".format(src)}
         ).fetchall()
-        destSystems = staticdata.execute(
+        dest_systems = staticdata.execute(
             """SELECT solarSystemID, solarSystemName
                FROM mapSolarSystems
                WHERE solarSystemName LIKE :name;""",
@@ -185,26 +182,27 @@ class Wormhole(object):
         ).fetchall()
         staticdata.close()
 
-        if not srcSystems or not destSystems:
+        if not src_systems or not dest_systems:
             return "Can't find matching systems!"
 
         # Sort by length of name so that the most similar item is first
-        srcSystems.sort(cmp=lambda x, y: cmp(len(x), len(y)), key=lambda x: x[1])
-        destSystems.sort(cmp=lambda x, y: cmp(len(x), len(y)), key=lambda x: x[1])
+        src_systems.sort(cmp=lambda x, y: cmp(len(x), len(y)), key=lambda x: x[1])
+        dest_systems.sort(cmp=lambda x, y: cmp(len(x), len(y)), key=lambda x: x[1])
 
         conn.execute(
             """INSERT INTO `connections` (SRC, `SRC-SIG`, DEST, `DEST-SIG`, expiry, author)
                VALUES (:srcID, :srcSIG, :destID, :destSIG,
-                       DATETIME("now", "{:+} hours"), :author);""".format(TTL),
-            {'srcID': srcSystems[0][0], 'srcSIG': srcSIG,
-             'destID': destSystems[0][0], 'destSIG': destSIG,
+                       DATETIME("now", "{:+} hours"), :author);""".format(ttl),
+            {'srcID': src_systems[0][0], 'srcSIG': src_sig,
+             'destID': dest_systems[0][0], 'destSIG': dest_sig,
              'author': self.get_uname_from_mess(mess)}
         )
         conn.commit()
         conn.close()
 
-        return "Wormhole from {} to {} was added by {}".format(srcSystems[0][1], destSystems[0][1],
-                                                               self.get_uname_from_mess(mess))
+        return "Wormhole from {} to {} was added by {}".format(
+            src_systems[0][1], dest_systems[0][1], self.get_uname_from_mess(mess)
+        )
 
     def wh_stats(self, mess):
         conn = self.__db_connection()
