d8e990a0d7b5eff753a1c450f2056c4a9780042f
TheJokr <leo@dashsec.com>
2016-06-11 13:50:02 +0200

    Added docstring to vmbot.botcmd

--- vmbot/botcmd.py
+++ vmbot/botcmd.py
@@ -4,6 +4,8 @@ from . import jabberbot
 
 
 def botcmd(*args, **kwargs):
+    """Mark decorated function as bot command."""
+
     def decorate(func, force_pm=False, **kwargs):
         setattr(func, "_vmbot_forcepm", force_pm)
         return jabberbot.botcmd(func, **kwargs)
