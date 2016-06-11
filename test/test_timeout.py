f5937a02345223573b3da159b4677aa816985fae
TheJokr <leo@dashsec.com>
2016-06-11 21:27:41 +0200

    Added tests for timeout decorator

---
+++ test/test_timeout.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+import unittest
+
+import time
+import signal
+
+from vmbot.helpers.exceptions import TimeoutError
+
+from vmbot.helpers.decorators import timeout
+
+
+@timeout(1)
+def timeout_success():
+    return True
+
+
+@timeout(1)
+def timeout_fail():
+    time.sleep(3)
+    return True
+
+
+@timeout(1, "TestException")
+def timeout_msg():
+    time.sleep(3)
+    return True
+
+
+@unittest.skipUnless(hasattr(signal, "alarm"), "OS doesn't support SIGALRM")
+class TestFormat(unittest.TestCase):
+    def test_timeout(self):
+        self.assertRaises(TimeoutError, timeout_fail)
+
+    def test_timeout_notimeout(self):
+        self.assertTrue(timeout_success())
+
+    def test_timeout_msg(self):
+        self.assertRaisesRegexp(TimeoutError, "TestException", timeout_msg)
+
+
+if __name__ == "__main__":
+    unittest.main()
