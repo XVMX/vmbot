XVMX/vmbot

commit b85f861c5d9aae8983114c071af21ea799bf682a

Fixed index search bug in news_feed
The index search is supposed to find the first known article.
If all articles are newer than the last_update timestamp,
all articles should be returned instead of none.

tools/cron/news_feed.py
@@ -98,6 +98,6 @@ def read_feed(url, last_entry):
         # Fallback in case CCP deletes the last entry
         last_update = time.strptime(last_update, "%Y-%m-%dT%H:%M:%SZ")
         idx = next((idx for idx, entry in enumerate(entries)
-                    if time.strptime(entry['updated'], "%Y-%m-%dT%H:%M:%SZ") <= last_update), 0)
+                    if time.strptime(entry['updated'], "%Y-%m-%dT%H:%M:%SZ") >= last_update), None)
 
     return entries[:idx]

commit 89afb7c7e751cac2c949422c2b7103e66f3af941

Fixed index search bug in cron feeds
This was accidentally broken in b85f861.
Older dates are ordered lower than the current one.

tools/cron/evemail.py
@@ -133,7 +133,7 @@ def filter_mails(last_mail, mails):
         # Fallback in case last_mail was deleted
         last_update = time.strptime(last_update, "%Y-%m-%dT%H:%M:%SZ")
         idx = next((idx for idx, mail in enumerate(mails)
-                    if time.strptime(mail['timestamp'], "%Y-%m-%dT%H:%M:%SZ") >= last_update), None)
+                    if time.strptime(mail['timestamp'], "%Y-%m-%dT%H:%M:%SZ") <= last_update), None)
 
     return mails[:idx]
 

tools/cron/news_feed.py
@@ -98,6 +98,6 @@ def read_feed(url, last_entry):
         # Fallback in case CCP deletes the last entry
         last_update = time.strptime(last_update, "%Y-%m-%dT%H:%M:%SZ")
         idx = next((idx for idx, entry in enumerate(entries)
-                    if time.strptime(entry['updated'], "%Y-%m-%dT%H:%M:%SZ") >= last_update), None)
+                    if time.strptime(entry['updated'], "%Y-%m-%dT%H:%M:%SZ") <= last_update), None)
 
     return entries[:idx]