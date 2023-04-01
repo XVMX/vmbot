8042c790b222080fe0d003445e0b78b2f9e71787
TheJokr <leo@bloecher.dev>
2023-04-01 02:50:46 +0200

    Improve structure and DRY in create_staticdata.sh

--- tools/ci/staticdata/create_staticdata.sh
+++ tools/ci/staticdata/create_staticdata.sh
@@ -1,44 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
+fname="sqlite-latest.sqlite.bz2"
+
 etag_path="${1:-.ci-etag}"
-etag_file="$etag_path/sqlite-latest.sqlite.bz2.txt"
-
-echo "Downloading sqlite-latest.sqlite.bz2..."
 mkdir -p "$etag_path"
-touch "$etag_file"
-curl -fsSLO --etag-compare "$etag_file" --etag-save "${etag_file}.new" \
-     https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2
-mv "${etag_file}.new" "$etag_file"
+etag_file="$etag_path/$fname.txt"
 
-if [[ ! -s sqlite-latest.sqlite.bz2 ]]; then
+# Requires curl 7.70 or later for bug-free etag support (CI: ubuntu-22.04)
+echo "Downloading $fname..."
+curl -Z -fsSLO -m 600 --etag-compare "$etag_file" --etag-save "$etag_file" \
+     "https://www.fuzzwork.co.uk/dump/$fname" --next \
+     -fsSLO -m 120 "https://www.fuzzwork.co.uk/dump/$fname.md5"
+
+if [[ ! -s "$fname" ]]; then
   echo "staticdata.sqlite is up to date"
-  rm sqlite-latest.sqlite.bz2
+  rm -f "$fname"{,.md5}
   exit 0
 fi
 
-curl -fsSLO https://www.fuzzwork.co.uk/dump/sqlite-latest.sqlite.bz2.md5
-
 echo "Verifying MD5 checksum..."
-if ! md5sum -c sqlite-latest.sqlite.bz2.md5; then
-    rm sqlite-latest.sqlite.bz2 sqlite-latest.sqlite.bz2.md5
-    exit 1
+if ! md5sum -c "$fname.md5"; then
+  rm -f "$fname"{,.md5}
+  exit 1
 fi
 
-echo -e "\nExtracting sqlite-latest.sqlite..."
-bunzip2 -v sqlite-latest.sqlite.bz2
+fname="sqlite-latest.sqlite"
+echo "Extracting $fname..."
+bzip2 -dfv "$fname.bz2"
 
-echo "Creating new database..."
-for table in "chrFactions" "mapRegions" "mapConstellations" "mapSolarSystems" \
-             "staStations" "invNames" "invTypes"; do
-  sqlite3 sqlite-latest.sqlite ".dump $table" >> dump.sql
-done
-sqlite3 staticdata.sqlite < dump.sql
+echo -e "\nCreating new database..."
+sqlite3 "$fname" <<EOF | sqlite3 staticdata.sqlite
+.bail on
+.dump chrFactions
+.dump mapRegions
+.dump mapConstellations
+.dump mapSolarSystems
+.dump staStations
+.dump invNames
+.dump invTypes
+EOF
 
 # The full dogma attribute data is >20 MB, but we only need a tiny subset of it.
 # So instead we store that data in a non-standard table.
 sqlite3 staticdata.sqlite <<EOL
 .bail on
-ATTACH DATABASE 'sqlite-latest.sqlite' AS sde;
+ATTACH DATABASE '$fname' AS sde;
 BEGIN IMMEDIATE;
 CREATE TABLE main.market_structures (
   typeID INTEGER NOT NULL PRIMARY KEY
@@ -55,6 +61,5 @@ EOL
 chmod 444 staticdata.sqlite
 
 echo "Deleting temporary files..."
-rm dump.sql sqlite-latest.sqlite sqlite-latest.sqlite.bz2.md5
-
+rm "$fname"{,.bz2.md5}
 echo "Finished"
