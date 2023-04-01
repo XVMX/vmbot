895e66777e992e24df93b21550e7e8b4e5c7ab24
TheJokr <leo@bloecher.dev>
2023-04-01 02:53:27 +0200

    Update scripts for new GSF emote pack

--- tools/ci/staticdata/create_emotes.sh
+++ tools/ci/staticdata/create_emotes.sh
@@ -1,28 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eu
+fname="GSF_Emotes.zip"
+
 etag_path="${1:-.ci-etag}"
-etag_file="$etag_path/pidgin_nosmile.zip.txt"
-
-echo "Downloading pidgin_nosmile.zip..."
 mkdir -p "$etag_path"
-touch "$etag_file"
-curl -fsSLO --etag-compare "$etag_file" --etag-save "${etag_file}.new" \
-     https://s3.amazonaws.com/emotes.gbs.io/pidgin_nosmile.zip
-mv "${etag_file}.new" "$etag_file"
+etag_file="$etag_path/$fname.txt"
 
-if [[ ! -s pidgin_nosmile.zip ]]; then
+# Requires curl 7.70 or later for bug-free etag support (CI: ubuntu-22.04)
+echo "Downloading $fname..."
+curl -fsSLO -m 300 --etag-compare "$etag_file" --etag-save "$etag_file" \
+     "https://wiki.goonswarm.org/images/0/0f/$fname"
+
+if [[ ! -s "$fname" ]]; then
   echo "emotes.txt is up to date"
-  rm pidgin_nosmile.zip
+  rm -f "$fname"
   exit 0
 fi
 
 # Requires GNU sed extensions
 echo "Extracting emotes.txt..."
-unzip -p pidgin_nosmile.zip "pidgin_nosmile/theme" |
+unzip -p "$fname" "pidgin/gsf/theme" |
   sed '0,/^\[default\]$/Id' > emotes.txt
 chmod 444 emotes.txt
 
-echo "Deleting pidgin_nosmile.zip..."
-rm pidgin_nosmile.zip
-
+echo "Deleting $fname..."
+rm "$fname"
 echo "Finished"
